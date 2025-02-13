5f41a05890290b7a465843d92bd7a8c87ae075b7
notBers <109546794+notBers@users.noreply.github.com>
2025-02-13 13:03:34 -0600

    estructura

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,22 @@
+package frc.robot;
+
+import frc.robot.commands.Autos;
+import edu.wpi.first.wpilibj2.command.Command;
+
+
+
+public class RobotContainer {
+  
+  public RobotContainer() {
+    configureBindings();
+  }
+
+  private void configureBindings() {
+
+  }
+
+  public Command getAutonomousCommand() {
+    return Autos.exampleAuto(null);
+  }
+
+}
